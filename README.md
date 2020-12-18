5bfb7bcabaa65d908fba6a808b551bffe6f065a2
Bryan Heinz <git@bryanheinz.com>
2020-12-18 19:48:34 +0000

    added list conversion

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # drafts-actions
 Repo to store my Drafts JavaScript actions.
+
+## Markdown to Dokuwiki
+### Numbered lists
+This converter treats '-' as ordered lists and '*' as unordered lists.
+
+I dislike the use of numbers for ordered lists in markdown unless I'm actually referencing the raw markdown. I find it really annoying when i have to rearrange an ordered list. This is my biggest (only?) gripe with Gruber's original markdown syntax - that he gave unordered lists two ways to be defined, but only gave ordered lists the numbered option.
--- markdownToDokuwiki.js
+++ markdownToDokuwiki.js
@@ -26,10 +26,30 @@ function convertHeader(match, groupA, groupB) {
     
     return hn + ' ' + groupB + ' ' + hn;
 }
+
 function convertLinks(a, b, c) {
     const dokuLink = "[[" + c + "|" + b + "]]";
     return dokuLink;
 }
+
+function convertNestLists(a) {
+    // split to get beginning spaces
+    let listDel = ''
+    if (a.includes('-')) {
+        listDel = '- '
+    } else {
+        listDel = '* '
+    }
+    const aList = a.split(listDel)[0]
+    // count number of spaces before -
+    const nestSpaces = aList.length
+    // markdown spaces are 4, dokuwiki are 2 spaces
+    // dividing by 4 let's us know how nested it is
+    // take how nested it is and times it by 2 spaces
+    const dokuSpaces = nestSpaces / 4 * 2
+    return ' '.repeat(dokuSpaces) + listDel
+}
+
 
 // convert beginning of italics
 let nt = selected_text.replace(/\b_/g, '//');
@@ -47,5 +67,19 @@ nt = nt.replace(/\b`{1}/g, "'' ");
 nt = nt.replace(/(#+) (.+)/g, convertHeader);
 // convert links
 nt = nt.replace(/\[(.*)\]\((.*)\)/g, convertLinks);
+// convert nested ordered list items
+// NOTE: this needs to come before top-level, otherwise this will also grab the
+// top-level items.
+// NOTE: i use - in MD instaled of numbers (1.). see the readme for reasons.
+nt = nt.replace(/^\s+- /gm, convertNestLists)
+// convert top-level ordered list items
+nt = nt.replace(/^- /gm, '  - ')
+// convert nested unordered list items
+// NOTE: this needs to come before top-level, otherwise this will also grab the
+// top-level items.
+nt = nt.replace(/^\s+\* /gm, convertNestLists)
+// convert top-level unordered list items
+nt = nt.replace(/^\* /gm, '  * ')
+// TODO: add numeric ordered list conversion
 
 app.setClipboard(nt);
